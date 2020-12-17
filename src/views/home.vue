1da673bd1efa732371f16f882f7410de592059a3
王水胜 <602466011@qq.com>
2020-12-17 14:32:09 +0800

    添加: home.vue

---
+++ src/views/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    {{ isOpen }}
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {
+    },
+    data() {
+      return {
+          
+      }
+    },
+    computed: {
+      isOpen() {
+        return this.$store.state.common.isOpen
+      }
+    },
+    watch: {},
+    created() {},
+    mounted() {
+    },
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filters: {}
+  }
+</script>
+<style scoped>
+</style>
